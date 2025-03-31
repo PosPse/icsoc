a2ba1ff38d7a79ced38daab9a4dff9bad8c84256
PoPse <1967439427@qq.com>
2025-03-31 14:29:02 +0800

    指令微调数据

--- datasets/data_process.py
+++ datasets/data_process.py
@@ -7,6 +7,9 @@ from prompt import (
     BusinessProcessDescriptionGeneration, 
     ActorExtraction,
     ActivityExtraction,
+    DataObjectExtraction,
+    TextAnnotationExtraction,
+    InstructionTuning
     )
 
 @dataclass
@@ -41,16 +44,14 @@ class Data:
     title: str
 
 class DataProcess:
-    def __init__(self, data_path):
-        self.data_path = data_path
-        self.raw_data = self.load_data()
-        
+    def __init__(self, raw_data_path, xml_data_path):
+        self._raw_data = self._load_data(raw_data_path)
+        self._xml_data = self._load_data(xml_data_path)
 
-    def load_data(self):
-        raw_data = json.load(open(self.data_path, "r"))
-        return raw_data
+    def _load_data(self, data_path):
+        return json.load(open(data_path, "r"))
     
-    def formatting_prompts_func(self, system="", instruction="", input="", output="") -> dict:
+    def _formatting_prompts_func(self, system="", instruction="", input="", output="") -> dict:
         return {
             "system": system,
             "instruction": instruction,
@@ -58,10 +59,12 @@ class DataProcess:
             "output": output
         }
     
-    def str_formatting(self, str):
+    def _str_formatting(self, str):
         return str.replace("\n", " ")
     
-    def extract_metadata(self, data: Data) -> tuple[str, str, list[str], list[str]]:
+    def _extract_from_raw(self, data: Data) -> tuple[str, str, list[str], list[str]]:
+        # 0. Extract file_index
+        file_index = data.file_index
         # 1. Extract title
         title = data.title
         # 2. Extract paragraph
@@ -72,45 +75,96 @@ class DataProcess:
         text_nodes = data.text_nodes
 
         # 3. Extract actors
-        actors = []
-        for node in step_nodes:
-            actors.append(node.agent)
-        for node in data_nodes:
-            actors.append(node.agent)
-        for node in text_nodes:
-            actors.append(node.agent)
+        actors = [node.agent for node in step_nodes + data_nodes + text_nodes]
         actors = list(set(actors))
-        actors = [i for i in actors if i != ""]
+        actors = list(filter(lambda x: x != "", actors))
+        actors_len = len(actors)
 
         # 4. Extract activities
-        activities = []
-        
-        return title, paragraph, actors, activities
+        activity_nodes = filter(lambda x: x.type == "Activity", step_nodes)
+        activities = [] if actors_len == 0 else {}
+        if actors_len > 0:
+            for node in activity_nodes:
+                if node.agent not in activities:
+                    activities[node.agent] = []
+                activities[node.agent].append(node.NodeText)
+            activities = {k: list(set(v)) for k, v in activities.items()}
+        else:
+            for node in activity_nodes:
+                activities.append(node.NodeText)
+            activities = list(set(activities))
+
+        # 5. Extract DataObject
+        data_objects = [] if actors_len == 0 else {}
+        if actors_len > 0:
+            for node in data_nodes:
+                if node.agent not in data_objects:
+                    data_objects[node.agent] = []
+                data_objects[node.agent].append(node.NodeText)
+            data_objects = {k: list(set(v)) for k, v in data_objects.items()}
+        else:
+            for node in data_nodes:
+                data_objects.append(node.NodeText)
+            data_objects = list(set(data_objects))
+
+        # 6. Extract TextAnnotation
+        text_annotations = [] if actors_len == 0 else {}
+        if actors_len > 0:
+            for node in text_nodes:
+                if node.agent not in text_annotations:
+                    text_annotations[node.agent] = []
+                text_annotations[node.agent].append(node.NodeText)
+            text_annotations = {k: list(set(v)) for k, v in text_annotations.items()}
+        else:
+            for node in text_nodes:
+                text_annotations.append(node.NodeText)
+            text_annotations = list(set(text_annotations))
+
+        return file_index, title, paragraph, actors, activities, data_objects, text_annotations
     
-    def transfer_to_dialog_data(self, data: Data) -> list[dict]:
+    def _transfer_to_dialog_data(self, data: Data) -> list[dict]:
         example = []
-        title, paragraph, actors, activities = self.extract_metadata(data)
+        file_index, title, paragraph, actors, activities, data_objects, text_annotations = self._extract_from_raw(data)
         ## 1. Generate business process description
         system = System.system_prompt
         instruction = BusinessProcessDescriptionGeneration.instruction_prompt
         input = BusinessProcessDescriptionGeneration.input_prompt.format(title=title)
         output = BusinessProcessDescriptionGeneration.output_prompt.format(business_process_description=paragraph)
-        example.append(self.formatting_prompts_func(system, instruction, input, output))
+        example.append(self._formatting_prompts_func(system, instruction, input, output))
 
         ## 2. Extract actors
         instruction = ActorExtraction.instruction_prompt
-        output = ActorExtraction.output_prompt.format(actors=actors) if len(actors) > 0 else ActorExtraction.default_output_prompt
-        example.append(self.formatting_prompts_func(instruction=instruction, output=output))
+        output = ActorExtraction.output_prompt.format(actors=actors, num_actors=len(actors)) if len(actors) > 0 else ActorExtraction.default_output_prompt
+        example.append(self._formatting_prompts_func(instruction=instruction, output=output))
 
         ## 3. Extract activities
-        # instruction = ActivityExtraction.instruction_prompt
-        # output = ActivityExtraction.output_prompt.format(activities=activities) if len(activities) > 0 else ActivityExtraction.default_output_prompt
-        # example.append(self.formatting_prompts_func(instruction=instruction, output=output))
+        instruction = ActivityExtraction.instruction_prompt
+        output = ActivityExtraction.output_prompt.format(activities=activities)
+        example.append(self._formatting_prompts_func(instruction=instruction, output=output))
 
-        ## 
+        ## 4. Extract DataObjects
+        is_empty_data_objects = len(data_objects) == 0
+        instruction = DataObjectExtraction.instruction_prompt
+        output = DataObjectExtraction.output_prompt.format(data_objects=data_objects) if not is_empty_data_objects else DataObjectExtraction.default_output_prompt
+        example.append(self._formatting_prompts_func(instruction=instruction, output=output))
+
+        ## 5. Extract TextAnnotations
+        is_empty_text_annotations = len(text_annotations) == 0
+        instruction = TextAnnotationExtraction.instruction_prompt
+        output = TextAnnotationExtraction.output_prompt.format(text_annotations=text_annotations) if not is_empty_text_annotations else TextAnnotationExtraction.default_output_prompt
+        example.append(self._formatting_prompts_func(instruction=instruction, output=output))
         return example
     
-    def transfer_to_openai_sharegpt_data(self, data: list[dict]) -> dict:
+    def _transfer_to_instruction_data(self, data: list[dict]) -> dict:
+        file_index, title, paragraph, actors, activities, data_objects, text_annotations = self._extract_from_raw(data)
+        business_process_diagram = self._xml_data[str(file_index)]
+        system = InstructionTuning.system_prompt
+        instruction = InstructionTuning.instruction_prompt
+        input = InstructionTuning.input_prompt.format(business_process_description=paragraph)
+        output = InstructionTuning.output_prompt(actors, len(actors), activities, data_objects, text_annotations, business_process_diagram)
+        return self._formatting_prompts_func(system, instruction, input, output)
+    
+    def _transfer_to_openai_sharegpt_data(self, data: list[dict]) -> dict:
         dialog_data = []
         for d in data:
             if d["system"] != "":
@@ -121,12 +175,20 @@ class DataProcess:
 
     def build_dialog_tuning_dataset(self, save_path):
         openai_sharegpt_dataset = []
-        for data in tqdm(self.raw_data, desc="Dialog Dataset Processing: "):
+        for data in tqdm(self._raw_data, desc="Dialog Dataset Processing: "):
             data = from_dict(data_class=Data, data=data)
-            data = self.transfer_to_dialog_data(data)
-            data = self.transfer_to_openai_sharegpt_data(data)
+            data = self._transfer_to_dialog_data(data)
+            data = self._transfer_to_openai_sharegpt_data(data)
             openai_sharegpt_dataset.append(data)
         self.save_dataset(openai_sharegpt_dataset, save_path)
+
+    def build_instruction_tuning_dataset(self, save_path):
+        instruction_tuning_dataset = []
+        for data in tqdm(self._raw_data, desc="Instruction Dataset Processing: "):
+            data = from_dict(data_class=Data, data=data)
+            data = self._transfer_to_instruction_data(data)
+            instruction_tuning_dataset.append(data)
+        self.save_dataset(instruction_tuning_dataset, save_path)
         
     def save_dataset(self, custom_dataset, save_path):
         with open(save_path, "w") as f:
@@ -134,5 +196,7 @@ class DataProcess:
 
 if __name__ == "__main__":
     raw_data_path = "./PAGED/raw_data.json"
-    data_process = DataProcess(raw_data_path)
-    data_process.build_dialog_tuning_dataset(save_path="./PAGED/dialog_tuning_dataset.json")
+    xml_data_path = "./PAGED/xml_data.json"
+    data_process = DataProcess(raw_data_path, xml_data_path)
+    # data_process.build_dialog_tuning_dataset(save_path="./PAGED/dialog_tuning_dataset.json")
+    data_process.build_instruction_tuning_dataset(save_path="./PAGED/instruction_tuning_dataset.json")
--- datasets/prompt.py
+++ datasets/prompt.py
@@ -3,17 +3,53 @@ class System:
 
 class BusinessProcessDescriptionGeneration:
     instruction_prompt = "You will be given a title of business process description, and you need to generate a detailed business process description based on the title."
-    input_prompt = "Title: {title}"
-    output_prompt = "Business Process Description: {business_process_description}"
+    input_prompt = "##Title:\n{title}"
+    output_prompt = "##Business Process Description:\n{business_process_description}"
     
 class ActorExtraction:
     instruction_prompt = "Extract all actors involved in the business process description."
-    output_prompt = "Actors: {actors}"
-    default_output_prompt = "There are no clear actors in this business process description."
+    output_prompt = "##Actors:\n{actors}\n##The number of actors is {num_actors}."
+    default_output_prompt = "##Actors:\nThere are no clear actors in this business process description. The number of actors is 0."
 
 class ActivityExtraction:
     instruction_prompt = "Extract the activities involved by all actors in the business process description."
-    output_prompt = "Activities: {activities}"
+    output_prompt = "##Activities:\n{activities}"
+
+class DataObjectExtraction:
+    instruction_prompt = "Extract the data dependencies involved by all activities in the business process description."
+    output_prompt = "##DataObjects:\n{data_objects}"
+    default_output_prompt = "##DataObjects:\nThere are no data dependencies in the business process description."
+
+class TextAnnotationExtraction:
+    instruction_prompt = "Extract the text annotations involved by all activities in the business process description."
+    output_prompt = "##TextAnnotations:\n{text_annotations}"
+    default_output_prompt = "##TextAnnotations:\nThere are no text annotations in the business process description."
+
+class InstructionTuning:
+    system_prompt = "You are an expert in business process management. You first thinks about the reasoning process in the mind and then provides the user with the answer."
+    instruction_prompt = "You will be given a business process description, and you need to extract the participants, activities, data objects, and text annotations involved in the business process description. At the same time, you need to analyze the logical relationships between them, including start, end, order, selection, exclusivity, inclusion, etc., and use BPMN like XML format to draw the final business process diagram."
+    input_prompt = "##Business Process Description:\n{business_process_description}"
     
+    @staticmethod
+    def output_prompt(actors, num_actors, activities, data_objects, text_annotations, business_process_diagram):
+        actors_prompt = ActorExtraction.output_prompt.format(actors=actors, num_actors=num_actors) if num_actors > 0 else ActorExtraction.default_output_prompt
+        activities_prompt = ActivityExtraction.output_prompt.format(activities=activities)
+        data_objects_prompt = DataObjectExtraction.output_prompt.format(data_objects=data_objects) if len(data_objects) > 0 else DataObjectExtraction.default_output_prompt
+        text_annotations_prompt = TextAnnotationExtraction.output_prompt.format(text_annotations=text_annotations) if len(text_annotations) > 0 else TextAnnotationExtraction.default_output_prompt
+        business_process_diagram_prompt = "##Business Process Diagram:\n" + business_process_diagram
+        return f'''{actors_prompt}\n\n
+{activities_prompt}\n\n
+{data_objects_prompt}\n\n
+{text_annotations_prompt}\n\n
+{business_process_diagram_prompt}'''
+
 if __name__ == "__main__":
-    print(BusinessProcessDescriptionGeneration.input_prompt.format(title="Business Process Description"))
+    print(InstructionTuning.output_prompt(
+        actors=["Actor1", "Actor2"],
+        num_actors=2,
+        activities=["Activity1", "Activity2"],
+        data_objects=["DataObject1", "DataObject2"],
+        text_annotations=["TextAnnotation1", "TextAnnotation2"],
+        business_process_diagram="""<bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="Definitions_1" targetNamespace="http://bpmn.io/schema/bpmn" xmlns:bpmn20="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:omgdc="http://www.omg.org/spec/DD/20100524/DC" xmlns:omgdi="http://www.omg.org/spec/DD/20100524/DI" id="Diagram_1" elementRe
+        """
+    ))
